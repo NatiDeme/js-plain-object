f8f6d41d49d00f8701a28cee507cb5fd58c1d9c5
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-05-23 16:37:46 +0300

    Add input to local storage

--- main.js
+++ main.js
@@ -1,55 +1,39 @@
 const books = [];
 const add = document.getElementById('add');
+const titlei = document.querySelector('.title');
+const authori = document.querySelector('#author');
+const bookForm = document.querySelector('.bookForm');
 
-// add.addEventListener(event)
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 
-    class Book {
-      constructor(title, author) {
-        this.title = title;
-        this.author = author;
-      }
-    };
-   
-    populateLocalStorage = () => {
-      const books = this.getBooks();
-      if (!books) {
-        localStorage.setItem('books', JSON.stringify([]));
-        return books || [];
-      };
-    }
+populateLocalStorage = () => {
+  const books = this.getBooks();
+  if (!books) {
+    localStorage.setItem('books', JSON.stringify([]));
+  }
+};
 
-const booksSection = document.getElementById('books');
-const booksList = document.createElement('div');
-booksList.className = 'list';
-let booksElement = '';
+getBooks = () => {
+  const books = JSON.parse(localStorage.getItem('books'));
+  return books || [];
+};
 
-// const newObject = {};
-// // eslint-disable-next-line no-unused-vars
-// function addElement() {
-//   newObject.title = document.getElementById('title').value;
-//   newObject.author = document.getElementById('author').value;
-//   books.push(newObject);
-//   for (let i = 0; i < books.length; i += 1) {
-//     booksElement = `
-              
-//               <p id="display-title">${books[i].title}</p>
-//               <p id="display-author">${books[i].author}</p>
-//               <button id="remove">Remove</button>
-//               <hr>
-              
-//     `;
-//   }
-// }
+addBook = (book) => {
+  const books = this.getBooks();
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+};
+}
 
-booksSection.appendChild(booksList);
-booksList.innerHTML += booksElement;
+const book = new Book(titlei.value, authori.value);
 
-
-const book = new Book(inputTitle.value, inputAuthor.value);
-
-bookForm.addEventListener("submit", (e) => {
+bookForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const book = new Book(inputTitle.value, inputAuthor.value);
+  const book = new Book(titlei.value, authori.value);
   const { title, author } = book;
   const id = Math.round(Math.random() * 10000000);
 
@@ -61,9 +45,8 @@ bookForm.addEventListener("submit", (e) => {
 
   if (title && author) {
     book.addBook(newObject);
-    bookForm.reset();
-    book.showBook();
+    // book.showBook();
   }
 });
 
-window.addEventListener("DOMContentLoaded", book.showBook());
+// window.addEventListener('DOMContentLoaded', book.showBook());
