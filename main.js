29098526167490b2b09f5dff4239bb31ac80153d
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-05-23 14:39:20 +0100

    Changes

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 const books = [];
-const add = document.getElementById('add');
+const add = document.getElementById("add");
 
 // add.addEventListener(event)
 
-    class Book {
-      constructor(title, author) {
-        this.title = title;
-        this.author = author;
-      }
-    };
-   
-    populateLocalStorage = () => {
-      const books = this.getBooks();
-      if (!books) {
-        localStorage.setItem('books', JSON.stringify([]));
-        return books || [];
-      };
-    }
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
 
-const booksSection = document.getElementById('books');
-const booksList = document.createElement('div');
-booksList.className = 'list';
-let booksElement = '';
+populateLocalStorage = () => {
+  const books = this.getBooks();
+  if (!books) {
+    localStorage.setItem("books", JSON.stringify([]));
+    return books || [];
+  }
+
+  getBooks = () => {
+    const books = JSON.parse(localStorage.getItem("books"));
+    return books || [];
+  };
+};
+
+const booksSection = document.getElementById("books");
+const booksList = document.createElement("div");
+booksList.className = "list";
+let booksElement = "";
 
 // const newObject = {};
 // // eslint-disable-next-line no-unused-vars
@@ -31,19 +36,18 @@ let booksElement = '';
 //   books.push(newObject);
 //   for (let i = 0; i < books.length; i += 1) {
 //     booksElement = `
-              
+
 //               <p id="display-title">${books[i].title}</p>
 //               <p id="display-author">${books[i].author}</p>
 //               <button id="remove">Remove</button>
 //               <hr>
-              
+
 //     `;
 //   }
 // }
 
 booksSection.appendChild(booksList);
 booksList.innerHTML += booksElement;
-
 
 const book = new Book(inputTitle.value, inputAuthor.value);
 
